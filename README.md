9ff8563a6644f2f0c276c1a991260230aa2d1189
Jane Bradley <jane@openlearning.com>
2020-02-06 22:19:26 +1100

    * added detection of circular references * added a verbose/debugging mode to print reasons for cell errors * added more test cases; and expected output * code cleanup * updated README

--- README.md
+++ README.md
@@ -1,14 +1,38 @@
+# Description
+
+Python script which parses a given CSV file and evaluates each cell by these rules:
+1. Each cell is an expression in [postfix notation](https://en.wikipedia.org/wiki/Reverse_Polish_notation)
+2. Each number or operation will always be separated by one or more spaces
+3. A cell can refer to another cell, via the LETTER NUMBER notation (A2, B4, etc - letters refer to columns, numbers to rows)
+4. Support the basic arithmetic operators +, -, *, /
+
+And outputs a csv file of the same dimensions (to **STDOUT**), where each cell is evaluated to its final value. If any cell is an invalid expression then **for that cell only** print `#ERR`
+
+# Usage
+```
+$ python spreadsheet.py <input_file>
+```
+
+Verbose/debugging mode
+```
+$ python spreadsheet.py <input_file> -d
+```
+
+Run test
+```
+$ diff expected.csv <(python spreadsheet.py test.csv)
+```
+
 # Code structure
-Simple-ish python script with a main method that parses the input csv file and then evaluates each cell in the "spreadsheet" and prints the output one row at a time
+- `main` method parses input csv file into a 2D array, then
+- calls `evaluate_spreadsheet`, which evaluates the expression in each cell and outputs a 2D array of final values,
+- which is then output to **STDOUT** in csv format
 
-Algorithm for evaluating the postfix expressions is basically copied from wikipedia, with the only addition being the need to evaluate cell references.
-
-All helper methods are pure functions.
+`evaluate_postfix_expression` follows the first algorithm outlined [here](https://en.wikipedia.org/wiki/Reverse_Polish_notation#Postfix_evaluation_algorithm); main addition being that a list of seen cell references is passed along with the spreadsheet data and the current expression to `evaluate_operand` to faciliate detection of circular references.
 
 # Limitations
-* ~~This implementation assumes cell references are a single letter followed by a single number~~
-* Does not detect circular references ...
-* Doesn't store any previously calculated cell values / references
+* This implementation assumes cell references are a single letter followed by a number (of one or more digits)
+* Might be optimised by storing previously calculated cell values / references in a lookup table?
 * Have taken some shortcuts here and there, and relied on throwing/catching of exceptions
 instead of properly validating input data etc
 * Haven't added much test data beyond the examples given; the first things that came to mind were divide by zero error and circular references. Pretty much trusting that the algorithm from wikipedia is correct.
--- spreadsheet.py
+++ spreadsheet.py
@@ -5,38 +5,70 @@ import sys
 CELL_ERROR = '#ERR'
 OPERATORS = ['+', '-', '*', '/']
 
+class SpreadsheetException(Exception):
+    pass
+
 def main(argv):
     try:
         input_file_path = argv[0]
     except IndexError:
-        print 'usage: spreadsheet.py <inputfile>'
+        print('usage: spreadsheet.py <input_file>')
         sys.exit()
 
-    data = []
-
     # read input
+    data = []
     with open(input_file_path) as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         for row in csv_reader:
             data.append(row)
 
-    # evaluate and print output
-    for row, row_data in enumerate(data, start=1):
-        results = []
-        for i in range(0, len(row_data)):
-            col = chr(ord('a') + i) # map 0, 1, 2,... to 'a', 'b', 'c',...
-            cell_ref = '{}{}'.format(col, row)
+    # Hidden "option" for debugging purposes
+    include_reason = len(argv) > 1 and argv[1] == '-v'
+    results = evaluate_spreadsheet(data, include_reason)
+    output_csv(results)
+
+
+def output_csv(data):
+    # type: (Dict[Dict[str]]) -> None
+    for row in data:
+        print(','.join(row))
+
+
+def evaluate_spreadsheet(data, include_reason=False):
+    # type: (Dict[Dict[str]]) -> Dict[Dict[str]]
+
+    def value_to_string(value):
+        if isinstance(value, float) and value.is_integer():
+            return str(int(value)) # to format 1.0 as '1'
+        return str(value)
+
+    evaluated_data = []
+    for row_index, row_data in enumerate(data, start=0):
+        evaluated_row = []
+        for col_index, expression in enumerate(row_data, start=0):
+            cell_ref = map_location_to_cell_ref(row_index, col_index)
             try:
-                result = evaluate_cell(data, cell_ref)
-                # Slight hack to match output format of '1' vs '1.0'
-                if isinstance(result, float) and result.is_integer():
-                    result = int(result)
-                result = str(result)
-            except Exception as e:
-                # raise e
-                result = CELL_ERROR
-            results.append(result)
-        print ','.join(results)
+                value = evaluate_postfix_expression(data, expression, [cell_ref])
+                evaluated_row.append(value_to_string(value))
+            except SpreadsheetException as e:
+                evaluated_row.append(
+                    '{} ({})'.format(CELL_ERROR, e.message) if include_reason else CELL_ERROR
+                )
+        evaluated_data.append(evaluated_row)
+
+    return evaluated_data
+
+
+def map_location_to_cell_ref(row_index, col_index):
+    # type: (int, int) -> str
+
+    # map [0, 1, 2, ...] => [a, b, c, ...]
+    col_letter = chr(ord('a') + col_index)
+
+    # map [0, 1, 2, ...] => [1, 2, 3, ...]
+    row_number = row_index + 1
+
+    return '{}{}'.format(col_letter, row_number)
 
 
 '''
@@ -52,60 +84,29 @@ Adapted from https://en.wikipedia.org/wiki/Reverse_Polish_notation#Postfix_evalu
             push token onto the stack
     result <= pop from the stack
 '''
-def evaluate_cell(data, ref):
-    # type: (Dict[Dict[str]], str) -> float
+def evaluate_postfix_expression(data, expression, cell_references):
+    # type: (Dict[Dict[str]], str, List[str]) -> float
 
     stack = []
-    expression = get_cell(data, ref)
     tokens = expression.lower().split() # split() splits on whitespace by default
     for token in tokens:
         if token in OPERATORS:
-            operand2 = stack.pop()
-            operand1 = stack.pop()
+            try:
+                operand2 = stack.pop()
+                operand1 = stack.pop()
+            except IndexError:
+                raise SpreadsheetException('Invalid expression: not enough operands for operator')
+
             # Note: for this problem, we are only handling a few binary operators
             stack.append(evaluate_operation(operand1, operand2, token))
-        elif token == ref:
-            raise Exception('Self-referential cell found')
         else:
-            stack.append(evaluate_operand(data, token))
+            stack.append(evaluate_operand(data, token, cell_references))
 
     if len(stack) == 1:
         result = stack.pop()
-        # print('{} = {}'.format(expression, result))
         return result
 
-    raise Exception('Invalid expression in cell: {}'.format(ref))
-
-
-def get_cell(data, ref):
-    # type: (Dict[Dict[str]], str) -> str
-
-    '''
-    Assumes that cell references are one or more letters followed by one or more digits
-    '''
-    matches = re.match(r"^([^\W\d_-]+)(\d+)$", ref)
-    if matches:
-        col = ord(matches.group(1).lower()) - ord('a')
-        row = int(matches.group(2)) - 1
-        return data[row][col]
-
-    raise Exception("Invalid cell reference")
-
-def evaluate_operand(data, operand):
-    # type: (Dict[Dict[str]], str) -> float
-
-    '''
-    Assumes operands are either a number or a cell reference
-    '''
-
-    # Naive regex to match "numerical" inputs like:
-    # positive/negative whole/decimal numbers OR exponentials
-    # e.g. 1, -1, 0.9, -1.1, -1.0E+1
-    if re.match(r"-?\d+", operand):
-        return float(operand)
-
-    # probably a cell reference
-    return evaluate_cell(data, operand)
+    raise SpreadsheetException('Invalid expression')
 
 
 def evaluate_operation(operand1, operand2, operator):
@@ -118,8 +119,64 @@ def evaluate_operation(operand1, operand2, operator):
     if operator == '*':
         return operand1 * operand2
     if operator == '/':
-        return operand1 / operand2
-    raise Exception('Invalid operator found')
+        try:
+            return operand1 / operand2
+        except ZeroDivisionError:
+            raise SpreadsheetException('Division by zero')
+
+    raise SpreadsheetException('Invalid operator found')
+
+
+def evaluate_operand(data, operand, cell_references):
+    # type: (Dict[Dict[str]], str, List[str]) -> float
+
+    '''
+    Assumes operands are either a number or a cell reference
+    '''
+
+    # Check if this might be a circular reference
+    if operand in cell_references:
+        raise SpreadsheetException(
+            'Circular reference found: {}'.format(' => '.join(cell_references + [operand]))
+        )
+
+    # Naive regex to match "numerical" inputs like:
+    # positive/negative whole/decimal numbers OR exponentials
+    # e.g. 1, -1, 0.9, -1.1, -1.0E+1
+    if re.match(r"-?\d+", operand):
+        return float(operand)
+
+    # else should be a cell reference
+    (row_index, col_index) = map_cell_ref_to_location(operand)
+    try:
+        expression = data[row_index][col_index]
+    except IndexError:
+        raise SpreadsheetException('Reference to empty cell')
+
+    return evaluate_postfix_expression(data, expression, cell_references + [operand])
+
+
+def map_cell_ref_to_location(cell_ref):
+     # type: (str) -> (int, int)
+
+    '''
+    Cell references follow LETTER NUMBER notation
+    '''
+
+    matches = re.match(r"^([a-z]+)([1-9][0-9]*)$", cell_ref.lower())
+    if matches:
+        row_index = int(matches.group(2)) - 1
+
+        if len(matches.group(1)) > 1:
+            # TODO: handle columns past Z, e.g. as in Excel/Sheets where:
+            # * AA => column 27
+            # * AB => column 28
+            raise SpreadsheetException('Not implemented: Non-single letter cell references')
+
+        col_index = ord(matches.group(1)) - ord('a')
+        return (row_index, col_index)
+
+    raise SpreadsheetException("Invalid cell reference")
 
 
 if __name__ == "__main__":
